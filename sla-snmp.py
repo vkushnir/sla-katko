1035d7ac1380fa35c797749eccb18aa594573023
Vladimir Kushnir <vkushnir@gmail.com>
2023-02-05 12:51:50 +0500

    first working version

--- sla-snmp.py
+++ sla-snmp.py
@@ -3,16 +3,15 @@
 import logging
 import sys
 from argparse import ArgumentParser, Action, OPTIONAL
-
-from pysnmp.hlapi import *
-from pysnmp.entity.rfc3413.oneliner import cmdgen
+from subprocess import Popen, PIPE
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-rttMonCtrlAdminTag = ".1.3.6.1.4.1.9.9.42.1.2.1.1.3"
-rttMonCtrlAdminStatus = ".1.3.6.1.4.1.9.9.42.1.2.1.1.9"
-rttActive = "1"
+rttMonCtrlAdminTag = "iso.3.6.1.4.1.9.9.42.1.2.1.1.3"
+rttMonCtrlAdminStatus = "iso.3.6.1.4.1.9.9.42.1.2.1.1.9"
+rttActive = 1
+snmpOpts = "-OQ"
 
 
 def get_args():
@@ -20,12 +19,14 @@ def get_args():
     grps = parser.add_argument_group('Server')
     grps.add_argument('--server', required=True, action='store', help='SLA server')
     grps.add_argument('--server-port', action='store', help='Server port', default=161)
+    grps.add_argument('--server-snmp-version', action='store', help='Server SNMP version', default="2c")
     grps.add_argument('--server-read-community', action='store',
                       help='Server read community', default='public')
     grps.add_argument('--tag', required=True, action='store', help='SLA tag')
     grpc = parser.add_argument_group('Client')
     grpc.add_argument('--client', required=True, action='store', help='Client IP')
     grpc.add_argument('--client-port', action='store', help='Client port', default=161)
+    grpc.add_argument('--client-snmp-version', action='store', help='Client SNMP version', default="2c")
     grpc.add_argument('--client-read-community', action='store',
                       help='Client read community', default='public')
     grpc.add_argument('--client-write-community', action='store',
@@ -88,9 +89,9 @@ class _AppendOid(Action):
         elif len(values) == 2:
             oid, val = values
             if val.isdigit():
-                t = 'I'
+                t = 'i'
             else:
-                t = 'S'
+                t = 's'
         else:
             oid, t, val = values
         
@@ -100,67 +101,118 @@ class _AppendOid(Action):
         setattr(namespace, self.dest, items)
 
 
-def get_snmp_data(host, port, community, oid):
-    errorIndication, errorStatus, errorIndex, varBinds = next(
-        getCmd(SnmpEngine(),
-               CommunityData(community),
-               UdpTransportTarget((host, port)),
-               ContextData(),
-               ObjectType(ObjectIdentity(oid)))
-    )
-    
-    if errorIndication:
-        print(errorIndication)
-    elif errorStatus:
-        print('%s at %s' % (
-            errorStatus.prettyPrint(),
-            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'
-        ))
-    else:
-        for varBind in varBinds:
-            return varBind[1]
+def parse_snmp_data(data):
+    result = {}
+    for line in data.splitlines():
+        if line.startswith('iso'):
+            oid, value = line.split(' = ')
+            result[oid] = eval(value)
+    return result
 
-            
-def get_snmp_data_table(host, port, community, oid):
-    errorIndication, errorStatus, errorIndex, varBinds = next(
-        nextCmd(SnmpEngine(),
-                CommunityData(community),
-                UdpTransportTarget((host, port)),
-                ContextData(),
-                ObjectType(ObjectIdentity(oid)),
-                lexicographicMode=False)
-    )
-    
-    if errorIndication:
-        logger.error(errorIndication)
-    elif errorStatus:
-        logger.error('%s at %s' % (
-            errorStatus.prettyPrint(),
-            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'
-        ))
-    else:
-        for varBind in varBinds:
-            print(' = '.join([x.prettyPrint() for x in varBind]))
-        return varBinds
+
+def get_snmp_index(oid):
+    return oid.split('.')[-1]
+
+
+def get_snmp_data(args, oids, server='server'):
+    p = Popen(["snmpget", snmpOpts,
+               f"-v{getattr(args, f'{server}_snmp_version')}",
+               f"-c{getattr(args, f'{server}_read_community')}",
+               f"{getattr(args, f'{server}')}:{getattr(args, f'{server}_port')}"] + oids,
+              stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    if p.wait() != 0:
+        logger.error(f"Error: {stderr}")
+        sys.exit(1)
+    return stdout.decode('utf-8')
+
+
+def set_snmp_data(args, oids, server='server'):
+    p = Popen(["snmpset", snmpOpts,
+               f"-v{getattr(args, f'{server}_snmp_version')}",
+               f"-c{getattr(args, f'{server}_write_community')}",
+               f"{getattr(args, f'{server}')}:{getattr(args, f'{server}_port')}"] +
+              [value for oid in oids for value in oid.values()],
+              stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    if p.wait() != 0:
+        logger.error(f"Error: {stderr}")
+        sys.exit(1)
+    return stdout.decode('utf-8')
+
+
+def get_snmp_data_table(args, oids, server='server'):
+    p = Popen(["snmpbulkwalk", snmpOpts,
+               f"-v{getattr(args, f'{server}_snmp_version')}",
+               f"-c{getattr(args, f'{server}_read_community')}",
+               f"{getattr(args, f'{server}')}:{getattr(args, f'{server}_port')}"] + oids,
+              stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    if p.wait() != 0:
+        logger.error(f"Error: {stderr}")
+        sys.exit(1)
+    return stdout.decode('utf-8')
+
+
+def compare_snmp_data(oids, varBinds):
+    """Compare SNMP data with expected values, return True if all values are equal"""
+    equal = True
+    for oid in oids:
+        if oid['type'] == 'i':
+            if varBinds[oid['oid']] != int(oid['value']):
+                logger.error(
+                    f"OID {oid['oid']} value {varBinds[oid['oid']]} is not equal to {oid['value']}")
+                equal = False
+        elif oid['type'] == 's':
+            if varBinds[oid['oid']] != oid['value']:
+                logger.error(
+                    f"OID {oid['oid']} value {varBinds[oid['oid']]} is not equal to {oid['value']}")
+                equal = False
+    return equal
 
 
 def main(args):
     logger.info('Get data from server')
-    server_data = get_snmp_data_table(args.server, args.server_port, args.server_read_community, rttMonCtrlAdminTag)
-    test_data = get_snmp_data(args.server, args.server_port, args.server_read_community, ".1.3.6.1.4.1.9.9.42.1.2.1.1.3.50")
-    for varBind in server_data:
-        print(' = '.join([x.prettyPrint() for x in varBind]))
-    logger.debug('Server data: %s', server_data)
+    varBindsRttTag = parse_snmp_data(get_snmp_data_table(args, [rttMonCtrlAdminTag], 'server'))
+    logger.debug(f"rttMonCtrlAdminTag: {varBindsRttTag}")
+    for oid, value in varBindsRttTag.items():
+        if value == args.tag:
+            logger.info(f"Found tag {args.tag} in {oid}")
+            rttIndex = get_snmp_index(oid)
+            varBindsRttStatus = parse_snmp_data(get_snmp_data(args, [f"{rttMonCtrlAdminStatus}.{rttIndex}"], 'server'))
+            logger.debug(f"rttMonCtrlAdminStatus: {varBindsRttStatus}")
+            if varBindsRttStatus[f"{rttMonCtrlAdminStatus}.{rttIndex}"] == rttActive:
+                varBindsClient = parse_snmp_data(get_snmp_data(args, [oid['oid'] for oid in args.oid_ok], 'client'))
+                logger.debug(f"RTT status is OK, Client data: {varBindsClient}")
+                if not compare_snmp_data(args.oid_ok, varBindsClient):
+                    logger.info('RTT status is OK, Update client data')
+                    varBindsClientUpdate = set_snmp_data(args, args.oid_ok, 'client')
+                    logger.debug(f"Client data update: {varBindsClientUpdate}")
+                    if not compare_snmp_data(args.oid_ok, varBindsClientUpdate):
+                        logger.error('Update client data failed')
+                        sys.exit(1)
+            else:
+                varBindsClient = parse_snmp_data(get_snmp_data(args, [oid['oid'] for oid in args.oid_fail], 'client'))
+                logger.debug(f"RTT status is FAIL, Client data: {varBindsClient}")
+                if not compare_snmp_data(args.oid_fail, varBindsClient):
+                    logger.info('RTT status is FAIL, Update client data')
+                    set_snmp_data(args, args.oid_fail, 'client')
+                    varBindsClientUpdate = set_snmp_data(args, args.oid_fail, 'client')
+                    logger.debug(f"Client data update: {varBindsClientUpdate}")
+                    if not compare_snmp_data(args.oid_fail, varBindsClientUpdate):
+                        logger.error('Update client data failed')
+                        sys.exit(1)
+            break
 
 
 if __name__ == '__main__':
     """ SLA SNMP"""
-    ch = logging.StreamHandler(sys.stdout)
-    logger.addHandler(ch)
+    # ch = logging.StreamHandler(sys.stdout)
+    # logger.addHandler(ch)
     logger.default_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     
     arguments = get_args()
-
+    
     if arguments.log:
         fh = logging.FileHandler(arguments.log)
         logger.addHandler(fh)
